656adb5d2ccf0fba34214e4ffdeb9178c210a465
programmer-krish <s18002156@ousl.lk>
2022-09-06 17:43:48 +0530

    delete an employee

---
+++ src/controllers/employee.delete.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { log } from '../logger';
+import { ERRORS } from '../validation/error';
+import { deleteEmployee } from '../services/employee.delete.service';
+
+const deleteEmployeeById = async (req: Request, res: Response) => {
+    const id = req.params.empId;
+
+    try {
+        await deleteEmployee(id);
+        res.status(200).json({ message: 'Deleted Successfully' });
+    } catch (e) {
+        log.info(e);
+        return res.status(400).json({
+            status: 'error',
+            error: ERRORS.NOT_FOUND
+        });
+    }
+};
+
+export { deleteEmployeeById };
---
+++ src/services/employee.delete.service.ts
@@ -0,0 +1,15 @@
+import { Employee } from '../models/employee.model';
+import { log } from '../logger';
+
+const deleteEmployee = async (empId: string) => {
+    try {
+        const deletedEmployee = await Employee.deleteOne({ _id: empId });
+        if (deletedEmployee.deletedCount === 0) {
+            throw new Error('Could not find Employee.');
+        }
+    } catch (e) {
+        log.info(e);
+    }
+};
+
+export { deleteEmployee };
