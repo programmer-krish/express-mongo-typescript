60c383328618d12904ddb04714405d4f59a611d0
krishna-swivel <krishnamohan@swivelgroup.com.au>
2022-10-19 11:39:46 +0530

    removed unnecessary code base

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -8,20 +8,13 @@ const postSchema = Joi.object({
     photo: Joi.string()
 });
 
-const service = Joi.object().keys({
-    first_name: Joi.string().max(30).required(),
-    last_name: Joi.string().max(30).required(),
-    email: Joi.string().email().required(),
-    number: Joi.string(),
-    gender: Joi.string(),
-    photo: Joi.string()
-});
+
 
 const reqParamasSchemaId = Joi.object({
     id : Joi.string().id().required(),
 })
 
-const services = Joi.array().items(service);
+const services = Joi.array().items(postSchema);
 
 const responseSchema = Joi.object().keys({
     id: Joi.string(),
